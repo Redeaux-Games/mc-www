b650814b26725bd8fa1d6992aa2476f034b4a010
The Verse <floatbeta@gmail.com>
2023-02-01 17:58:46 -0600

    Add files via upload

---
+++ js/text.js
@@ -0,0 +1,25 @@
+const triggerRandomText = () => {
+    const letters = "䷊☲䷴☱䷣☷䷞☵䷁☶䷨⚍䷮䷀䷚䷉䷄䷱䷍⚎";
+    let interval = null;
+    let iteration = 0;
+    clearInterval(interval);
+    
+    interval = setInterval(() => {
+      document.querySelector("h5").innerText = document.querySelector("h5").innerText
+        .split("")
+        .map((letter, index) => {
+          if(index < iteration) {
+            return document.querySelector("h5").dataset.value[index];
+          }
+        
+          return letters[Math.floor(Math.random() * 20)];
+        })
+        .join("");
+      
+      if(iteration >= document.querySelector("h5").dataset.value.length){ 
+        clearInterval(interval);
+      }
+      
+      iteration += 1 / 3;
+    }, 50);
+}; 
