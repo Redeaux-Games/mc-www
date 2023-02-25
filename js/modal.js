94d5c533f6f967892fca0423015725223ccbcbfc
The Verse <floatbeta@gmail.com>
2023-02-25 00:37:53 -0600

    Test

--- js/modal.js
+++ js/modal.js
@@ -1,54 +1,57 @@
-                const launchButton = document.querySelector('#launch-modal');
-                const launchButtonA = document.querySelector('#launch-modal-a');
-                const launchButtonB = document.querySelector('#launch-modal-b');
-                const launchButtonC = document.querySelector('#launch-modal-c');
-                const modal = document.querySelector('#modal');
-                const closeButton = document.querySelector('.close-button');
-                const modalContent = document.querySelector('#modal-content');
+document.addEventListener('DOMContentLoaded', () => {
 
-                launchButton.addEventListener('click', () => {
-                fetch('../roadmap.html')
-                    .then((response) => response.text())
-                    .then((html) => {
-                    modalContent.innerHTML = html;
-                    modal.style.display = 'block';
-                    });
-                });
+  const launchButton = document.querySelector('#launch-modal');
+  const launchButtonA = document.querySelector('#launch-modal-a');
+  const launchButtonB = document.querySelector('#launch-modal-b');
+  const launchButtonC = document.querySelector('#launch-modal-c');
+  const modal = document.querySelector('#modal');
+  const closeButton = document.querySelector('.close-button');
+  const modalContent = document.querySelector('#modal-content');
 
-                launchButtonA.addEventListener('click', () => {
-                fetch('../cookies.html')
-                    .then((response) => response.text())
-                    .then((html) => {
-                    modalContent.innerHTML = html;
-                    modal.style.display = 'block';
-                    });
-                });
+  launchButton.addEventListener('click', () => {
+    fetch('../roadmap.html')
+      .then((response) => response.text())
+      .then((html) => {
+        modalContent.innerHTML = html;
+        modal.style.display = 'block';
+      });
+  });
 
-                launchButtonB.addEventListener('click', () => {
-                fetch('../terms.html')
-                    .then((response) => response.text())
-                    .then((html) => {
-                    modalContent.innerHTML = html;
-                    modal.style.display = 'block';
-                    });
-                });
+  launchButtonA.addEventListener('click', () => {
+    fetch('../cookies.html')
+      .then((response) => response.text())
+      .then((html) => {
+        modalContent.innerHTML = html;
+        modal.style.display = 'block';
+      });
+  });
 
-                launchButtonC.addEventListener('click', () => {
-                fetch('../privacy.html')
-                    .then((response) => response.text())
-                    .then((html) => {
-                    modalContent.innerHTML = html;
-                    modal.style.display = 'block';
-                    });
-                });
+  launchButtonB.addEventListener('click', () => {
+    fetch('../terms.html')
+      .then((response) => response.text())
+      .then((html) => {
+        modalContent.innerHTML = html;
+        modal.style.display = 'block';
+      });
+  });
 
+  launchButtonC.addEventListener('click', () => {
+    fetch('../privacy.html')
+      .then((response) => response.text())
+      .then((html) => {
+        modalContent.innerHTML = html;
+        modal.style.display = 'block';
+      });
+  });
 
-                closeButton.addEventListener('click', () => {
-                modal.style.display = 'none';
-                });
+  closeButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
 
-                window.addEventListener('click', (event) => {
-                if (event.target === modal) {
-                    modal.style.display = 'none';
-                }
-                });
+  window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
+
+});
