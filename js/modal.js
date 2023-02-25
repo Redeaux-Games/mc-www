ff51f2546bdfa9d5b8b7b26fdaa95ab1c2126e67
The Verse <floatbeta@gmail.com>
2023-02-24 23:44:42 -0600

    Update

---
+++ js/modal.js
@@ -0,0 +1,54 @@
+                const launchButton = document.querySelector('#launch-modal');
+                const launchButtonA = document.querySelector('#launch-modal-a');
+                const launchButtonB = document.querySelector('#launch-modal-b');
+                const launchButtonC = document.querySelector('#launch-modal-c');
+                const modal = document.querySelector('#modal');
+                const closeButton = document.querySelector('.close-button');
+                const modalContent = document.querySelector('#modal-content');
+
+                launchButton.addEventListener('click', () => {
+                fetch('../roadmap.html')
+                    .then((response) => response.text())
+                    .then((html) => {
+                    modalContent.innerHTML = html;
+                    modal.style.display = 'block';
+                    });
+                });
+
+                launchButtonA.addEventListener('click', () => {
+                fetch('../cookies.html')
+                    .then((response) => response.text())
+                    .then((html) => {
+                    modalContent.innerHTML = html;
+                    modal.style.display = 'block';
+                    });
+                });
+
+                launchButtonB.addEventListener('click', () => {
+                fetch('../terms.html')
+                    .then((response) => response.text())
+                    .then((html) => {
+                    modalContent.innerHTML = html;
+                    modal.style.display = 'block';
+                    });
+                });
+
+                launchButtonC.addEventListener('click', () => {
+                fetch('../privacy.html')
+                    .then((response) => response.text())
+                    .then((html) => {
+                    modalContent.innerHTML = html;
+                    modal.style.display = 'block';
+                    });
+                });
+
+
+                closeButton.addEventListener('click', () => {
+                modal.style.display = 'none';
+                });
+
+                window.addEventListener('click', (event) => {
+                if (event.target === modal) {
+                    modal.style.display = 'none';
+                }
+                });
